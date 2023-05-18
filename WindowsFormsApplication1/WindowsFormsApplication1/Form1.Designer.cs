9d098bcedc9c4da21864b029db40daca002b7bdc
sA1622 <101582943+sA1622@users.noreply.github.com>
2023-05-18 14:00:42 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1040, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1271, 487);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 487);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            /*System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Orange,20);
+            //Rectangle myRectangle = new Rectangle(20, 20,200,200);
+            //graphicsObj.DrawRectangle(myPen,myRectangle);
+            for (int i = 1; i <= 100; i++)
+            {
+                Rectangle myRectangle = new Rectangle(i, 20, i, 200);
+                graphicsObj.DrawEllipse(myPen, myRectangle);
+                System.Threading.Thread.Sleep(250);
+            }*/
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Blue, 50);
+            Point p1 = new Point(200, 200);
+            Point p2 = new Point(20, 100);
+            graphicsObj.DrawLine(myPen, p2, p1);
+        }
+
+        public void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            /*System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Orange, 50);
+            //Rectangle myRectangle = new Rectangle(20, 20,200,200);
+            //graphicsObj.DrawRectangle(myPen,myRectangle);
+            for (int i = 1; i <= 50; i++)
+            {
+                Rectangle myRectangle = new Rectangle(0, 0, i, i);
+                graphicsObj.DrawRectangle(myPen, myRectangle);
+                Rectangle myRectangle1 = new Rectangle(100, 100, i, i);
+                graphicsObj.DrawRectangle(myPen, myRectangle1);
+                Rectangle myRectangle2 = new Rectangle(200, 200, i, i);
+                graphicsObj.DrawRectangle(myPen, myRectangle2);
+                System.Threading.Thread.Sleep(100);
+            }
+            for (int i = 1; i <= 50; i++)
+            {
+                Rectangle myRectangle3 = new Rectangle(i, i, i, i);
+                graphicsObj.DrawEllipse(myPen, myRectangle3);
+            }*/
+            System.Drawing.Graphics myGraphics;
+            myGraphics = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Blue, 10);
+            for (int i = 1; i <= 100; i++)
+            {
+                Point p1 = new Point(i, 100);
+                Point p2 = new Point(100, i);
+                //panel1.Invoke(new (MethodInvoker)(()=> panel1.Graphics.DrawLine(myPen, p1, p2)));
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //backgroundWorker1.RunWorkerAsync();
+            //timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
+
+        public void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics myGraphics;
+            myGraphics = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Blue, 1);
+            int p12 = 100;
+            int p21 = 100;
+            /*for (int i = 100; i <= 200; i++)
+            {
+                Point p1 = new Point(i, 0);
+                Point p2 = new Point(0, i+300);
+                
+                e.Graphics.DrawLine(myPen, p1, p2);
+                //System.Threading.Thread.Sleep(100);
+            }*/
+            for (int i = 10; i < 1000; i+=10)
+            {
+                //double p1_= Math.Cos(i);
+                //double p2_ = Math.Sin(i);
+                Point p1 = new Point(0, 0);
+                Point p2 = new Point(i, 500);
+                e.Graphics.DrawLine(myPen, p1, p2);
+            }
+        }
+    }
+}
